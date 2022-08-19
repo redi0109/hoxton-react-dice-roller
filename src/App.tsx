931dfa4eddad7a2a311f6f1ee839fff90a8eacbd
Demir Boraj <demirboraj97@gmail.com>
2022-08-19 13:37:10 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  
+  const diceSides = 6;
+  const [dice, setDice] = useState(0)
 
 
   return (
     <div className="App">
     
+      <h1>Dice Roller</h1>
+      <h2>Roll the dice</h2>
+      <button onClick={() => setDice(Math.floor(Math.random() * diceSides) + 1)}>Roll</button>
+      <h2>{dice}</h2>
+
+
+
     </div>
   )
 }
